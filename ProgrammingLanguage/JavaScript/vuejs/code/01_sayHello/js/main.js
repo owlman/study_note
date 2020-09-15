3a7bc2618a6deb1aedb8f574bf501b7b4325047b
owlman <jie.owl2008@gmail.com>
2020-09-15 14:08:54 +0800

    vuejs笔记：sayHello示例

---
+++ ProgrammingLanguage/JavaScript/vuejs/code/01_sayHello/js/main.js
@@ -0,0 +1,12 @@
+// 程序名称： sayHello
+// 实现目标：
+//   1. 验证 Vue.js 执行环境
+//   2. 体验构建 Vue.js 程序的基本步骤
+
+const app = new Vue({
+    el: '#app',
+    data:{
+        sayHello: '你好，Vue.js！',
+        vueLogo: 'img/logo.png'
+    }
+})
