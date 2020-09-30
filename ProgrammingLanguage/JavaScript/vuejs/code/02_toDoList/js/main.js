9555eae01705bdaaddb6787c3500b1b9b6d38037
owlman <jie.owl2008@gmail.com>
2020-09-30 10:49:33 +0800

    完成02_todoList

--- ProgrammingLanguage/JavaScript/vuejs/code/02_toDoList/js/main.js
+++ ProgrammingLanguage/JavaScript/vuejs/code/02_toDoList/js/main.js
@@ -1,7 +1,7 @@
 // 程序名称： toDoList
 // 实现目标：
-//   1. 学习 v-if、v-for 等指令
-//   2. 体验如何处理用户输入
+//   1. 学习 v-model、v-for 等指令
+//   2. 掌握如何处理用户输入
 
 const app = new Vue({
     el: '#app',
@@ -11,9 +11,16 @@ const app = new Vue({
         doneList: []
     },
     methods:{
-        todo: function() {
-                if(this.newTask !== '')
-                  this.taskList.push(this.newTask);
-              }
+        addNew: function() {
+                  if(this.newTask !== '') {
+                      this.taskList.push(this.newTask);
+                      this.newTask = '';
+                  }
+              },
+        remove: function(index) {
+                  if(index >=  0) {
+                      this.taskList.splice(index,1);
+                  }
+                }
     }
 });
