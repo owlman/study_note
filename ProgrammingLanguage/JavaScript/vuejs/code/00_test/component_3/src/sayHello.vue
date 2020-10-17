9219f4c83970a49d3479c1dd41c5b4c47c39f53c
owlman <jie.owl2008@gmail.com>
2020-10-17 17:15:00 +0800

    学习 vue 组件：转译与构建工具

---
+++ ProgrammingLanguage/JavaScript/vuejs/code/00_test/component_3/src/sayHello.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>你好， {{ you }}！</h1>
+        <input type="text" v-model="you" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'sayHello',
+        props : ['who'],
+        data : function() {
+            return {
+                you: this.who
+            }
+        }
+    };
+</script>
+
+<style>
+    /* 如有必要还可以设置一些样式 */
+</style>
