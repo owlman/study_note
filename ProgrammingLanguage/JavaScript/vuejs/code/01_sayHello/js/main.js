c9c95f023616c67070519907d986bb11bede3117
owlman <jie.owl2008@gmail.com>
2021-03-19 17:53:32 +0800

    '更新'

--- ProgrammingLanguage/JavaScript/vuejs/code/01_sayHello/js/main.js
+++ ProgrammingLanguage/JavaScript/vuejs/code/01_sayHello/js/main.js
@@ -3,6 +3,12 @@
 //   1. 验证 Vue.js 执行环境
 //   2. 体验构建 Vue.js 程序的基本步骤
 
+// 加载开发环境版本，该版本包含了有帮助的命令行警告
+import Vue from '../node_modules/vue/dist/vue.esm.browser.js';
+// 或者
+// 加载生产环境版本，该版本优化了文件大小和载入速度
+// import Vue from '../node_modules/vue/dist/vue.esm.browser.min.js';
+
 const app = new Vue({
     el: '#app',
     data:{
@@ -16,3 +22,4 @@ const app = new Vue({
         }
     }
 });
+``
--- ProgrammingLanguage/JavaScript/vuejs/code/02_toDoList/js/main.js
+++ ProgrammingLanguage/JavaScript/vuejs/code/02_toDoList/js/main.js
@@ -3,6 +3,12 @@
 //   1. 学习 v-model、v-for 等指令
 //   2. 掌握如何处理用户输入
 
+// 加载开发环境版本，该版本包含了有帮助的命令行警告
+import Vue from '../node_modules/vue/dist/vue.esm.browser.js';
+// 或者
+// 加载生产环境版本，该版本优化了文件大小和载入速度
+// import Vue from '../node_modules/vue/dist/vue.esm.browser.min.js';
+
 const app = new Vue({
     el: '#app',
     data:{
