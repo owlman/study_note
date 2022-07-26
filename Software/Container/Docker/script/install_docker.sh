ee54a293e343802255f79c978fca193ba00f3c8b
owlman <jie.owl2008@gmail.com>
2022-07-26 14:34:07 +0800

    添加临时资料

---
+++ Software/Container/Docker/script/install_docker.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates 
+curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt -y install docker-ce docker-ce-cli containerd.io
