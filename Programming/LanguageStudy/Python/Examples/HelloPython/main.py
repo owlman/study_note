3149f744d16bb2a8215c6016294d043bf0f4b626
owlman <jie.owl2008@gmail.com>
2023-05-04 11:43:13 +0800

    '添加《Python快速入门笔记》'

---
+++ Programming/LanguageStudy/Python/Examples/HelloPython/main.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+'''
+    Created on 2023-3-1
+
+    @author: lingjie
+    @name : 01_HelloPython
+'''
+
+def main():
+    message = "This is an object-oriented,open-source programming language often used for rapid application development.Python's simple syntax amphasizes readability,reducing the cost of program mantenance, while its large library of functions and calls encourages reuse and extensibility."
+
+    print("Hello Python! \n", message)
+
+if (__name__ == "__main__"):
+    main()
+·   
