4f44d0b146128e2b2b4b144e6921e7ea17a60d21
owlman <jie.owl2008@gmail.com>
2023-05-28 15:35:07 +0800

    '更新rust笔记'

--- Programming/LanguageStudy/Rust/Examples/hello_world/src/main.rs
+++ Programming/LanguageStudy/Rust/Examples/hello_world/src/main.rs
@@ -1,16 +1,17 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
     let mut input = String::new();
-    println!("Please give me a name...");
+    print!("Please give me a name: ");
+    io::stdout().flush().expect("output error!");
     match io::stdin().read_line(&mut input) {
         Ok(n) => {
-            println!("{n} bytes read");
+            println!("{} bytes read", n);
             let name = input.trim();
-            println!("Hello, world! \nMy name is {name}.");
+            println!("Hello, world! \nMy name is {}.", name);
         }
         Err(e) => {
-            println!("Error Message: {e}");
+            println!("Error Message: {}", e);
         }
     }
 } 
