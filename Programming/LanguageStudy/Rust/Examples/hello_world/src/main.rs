8565caf2189a37aa3bcaf74ec20f32c1e5ed656c
owlman <jie.owl2008@gmail.com>
2023-06-03 15:46:00 +0800

    '更新rust笔记：安装开发环境'

--- Programming/LanguageStudy/Rust/Examples/hello_world/src/main.rs
+++ Programming/LanguageStudy/Rust/Examples/hello_world/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Write};
 
-    fn main() {
+fn main() {
     let mut input = String::new();
     print!("Please give me a name: ");
     io::stdout().flush().expect("output error!");
